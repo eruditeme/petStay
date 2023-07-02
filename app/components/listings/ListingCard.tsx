b2dc49afcde0214b054d5cd9099c1e3617228034
eruditeme <evangelineyiqin@gmail.com>
2023-07-02 04:59:54 -0400

    created listing card

---
+++ app/components/listings/ListingCard.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import useCountries from "@/app/hooks/useCountries";
+import { Listing, Reservation, User } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { useCallback, useMemo } from "react";
+import {format} from "date-fns";
+import Image from "next/image";
+
+interface ListingCardProps {
+    data: Listing;
+    reservation?: Reservation;
+    onAction?: (id: string) => void;
+    disabled?: boolean;
+    actionLabel?: string;
+    actionId?: string;
+    currentUser?: User | null;
+}
+
+const ListingCard: React.FC<ListingCardProps> = ({
+    data,
+    reservation,
+    onAction,
+    disabled,
+    actionLabel,
+    actionId = "",
+    currentUser
+}) => {
+    const router = useRouter();
+    const {getByValue} = useCountries();
+    const location = getByValue(data.locationValue);
+    const handleCancel = useCallback(
+        (e: React.MouseEvent<HTMLButtonElement>) => {
+            e.stopPropagation();
+
+            if (disabled) {
+                return;
+            }
+    
+            onAction?.(actionId);
+        }, [onAction, actionId, disabled]
+    )
+
+    const price = useMemo(() => {
+        if (reservation) {
+            return reservation.totalPrice;
+        }
+        return data.price;
+    }, [reservation, data.price])
+
+    const reservationDate = useMemo(() => {
+        if (!reservation) {
+            return null;
+        }
+        const start = new Date(reservation.startdate);
+        const end = new Date(reservation.endDate);
+
+        return `${format(start, "PP")} - ${format(end, "PP")}`
+    }, [reservation])
+    return (
+        <div onClick={() => router.push(`/listings/${data.id}`)} className="col-span-1 cursor-pointer group">
+            <div className="flex flex-col gap-2 w-full">
+                <div className="w-full relative overflow-hidden border-2 rounded-xl">
+                    <div className=" flex justify-center items-center py-4" style={{ backgroundImage: "url('/images/card_background.png')", backgroundSize: 'cover' }}>
+                        <Image 
+                            height={100}
+                            width={100}
+                            alt = "Listing"
+                            src= {data.image !== "" ? data.image : "/images/default.jpeg"}
+                            className="rounded-full"
+                        />
+                    </div>
+                    <hr></hr>
+                    <div className="p-4 xs: text-lg sm:text-md md:text-md lg:text-sm">
+                        {data.title}
+                        <div className="text-slate-500">
+                            {}
+                        </div>
+                        <div className="text-slate-500 my-4">
+                            {data.description}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default ListingCard;
